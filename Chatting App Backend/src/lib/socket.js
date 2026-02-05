705e9bfd566901d8274f45091d15425b1bf788e4
Shubham Kumar <shubhamkr5923@gmail.com>
2026-02-05 17:36:52 +0530

    Update socket.js

--- Chatting App Backend/src/lib/socket.js
+++ Chatting App Backend/src/lib/socket.js
@@ -1,3 +1,40 @@
+// import { Server } from "socket.io";
+// import http from "http";
+// import express from "express";
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: process.env.CLIENT_URL,
+//   },
+// });
+
+// export function getReceiverSocketId(userId) {
+//   return userSocketMap[userId];
+// }
+
+// // used to store online users
+// const userSocketMap = {}; // {userId: socketId}
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected", socket.id);
+
+//   const userId = socket.handshake.query.userId;
+//   if (userId) userSocketMap[userId] = socket.id;
+
+//   // io.emit() is used to send events to all the connected clients
+//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected", socket.id);
+//     delete userSocketMap[userId];
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//   });
+// });
+
+// export { io, app, server };
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
@@ -5,31 +42,39 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+// store online users
+const userSocketMap = {}; // { userId: socketId }
+
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
-  },
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
-
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+  console.log("✅ User connected:", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
+    console.log("❌ User disconnected:", socket.id);
+
+    if (userId) {
+      delete userSocketMap[userId];
+    }
+
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });

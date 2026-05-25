51bf4fa2e2b401ce1c2c0a3f9d1a16917ef43510
Shubham Kumar <shubhamkr5923@gmail.com>
2026-05-25 13:51:34 +0530

    Update axios.js

--- Chatting App Frontend/src/lib/axios.js
+++ Chatting App Frontend/src/lib/axios.js
@@ -1,10 +1,3 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-//   withCredentials: true,
-// });
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
